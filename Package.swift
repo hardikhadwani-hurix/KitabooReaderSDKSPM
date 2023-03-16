fde167731e74efcccb1e95da8d29cf2d1be0b730
hardikkumar.hadwani <hardikkumar.hadwani@hurix.com>
2023-03-16 23:15:00 +0530

    Resolved issues

--- Package.swift
+++ Package.swift
@@ -17,14 +17,14 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "Kitaboo_Reader_SDK", url: "https://github.com/hardikhadwani-hurix/KitabooReaderSDK/releases/download/1.0/Kitaboo_Reader_SDK.xcframework.zip", checksum: "01680833a5571693615f13a70c1aff55f528c184525a40cdc847d6edb577520b"),
+        .binaryTarget(name: "Kitaboo_Reader_SDK", url: "https://github.com/hardikhadwani-hurix/KitabooReaderSDKSPM/releases/download/1.0/Kitaboo_Reader_SDK.xcframework.zip", checksum: "01680833a5571693615f13a70c1aff55f528c184525a40cdc847d6edb577520b"),
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "KitabooReaderSDKSPM",
             dependencies: []),
         .testTarget(
-            name: "KitabooReaderSDKSPMTests",
+            name: "KitabooReaderSDKTests",
             dependencies: ["KitabooReaderSDKSPM"]),
     ]
 )
